3e912862f2c4dd1188ccffe3352ce59d724ac2df
bright <brittugu8@gmail.com>
2024-02-12 15:14:45 +0100

    Todo Details

--- lib/ui/home/create_timer.dart
+++ lib/ui/home/create_timer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_timer_app/ui/widgets/snackBar.dart';
 import 'package:simple_timer_app/utils/drawables/svg.dart';
 
 import '../../data/todo.dart';
@@ -22,7 +23,11 @@ class CreateTimer extends StatefulWidget {
 
 class _CreateTimerState extends State<CreateTimer> {
   List<String> projectitems = ["Project", "Task"];
-  List<String> task = ["Have a Meeting with Ceo", "Daily Scrum","Code Review with Dev"];
+  List<String> task = [
+    "Have a Meeting with Ceo",
+    "Daily Scrum",
+    "Code Review with Dev"
+  ];
 
   String? selecteditem;
 
@@ -33,15 +38,15 @@ class _CreateTimerState extends State<CreateTimer> {
   TextEditingController descriptiontext = TextEditingController();
 
   late bool isfav;
+
   @override
   void initState() {
     descriptiontext.text = widget.description;
 
     setState(() {
-      isfav =  false;
-      shouldshowfav =  false;
-      makeFav =  false;
-
+      isfav = false;
+      shouldshowfav = false;
+      makeFav = false;
     });
     super.initState();
   }
@@ -67,9 +72,11 @@ class _CreateTimerState extends State<CreateTimer> {
                 padding: const EdgeInsets.all(20.0),
                 child: Row(
                   children: [
-                    GestureDetector(onTap: (){
-                      Navigator.pop(context);
-                    },child: SvgPicture.asset(etback)),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: SvgPicture.asset(etback)),
                     Expanded(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -230,7 +237,9 @@ class _CreateTimerState extends State<CreateTimer> {
                 child: GestureDetector(
                   onTap: () {
                     setState(() {
-                      shouldshowfav == false ? shouldshowfav = true : shouldshowfav = false;
+                      shouldshowfav == false
+                          ? shouldshowfav = true
+                          : shouldshowfav = false;
                     });
                   },
                   child: Row(
@@ -250,7 +259,6 @@ class _CreateTimerState extends State<CreateTimer> {
                   ),
                 ),
               ),
-
               Visibility(
                 visible: shouldshowfav,
                 child: Padding(
@@ -280,16 +288,20 @@ class _CreateTimerState extends State<CreateTimer> {
                                       width: 24.0,
                                       height: 24.0,
                                       decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5.0),
-                                        border: Border.all(width: 1.0, color: Colors.white),
-                                        color: makeFav ? litblu : Colors.transparent,
+                                        borderRadius:
+                                            BorderRadius.circular(5.0),
+                                        border: Border.all(
+                                            width: 1.0, color: Colors.white),
+                                        color: makeFav
+                                            ? litblu
+                                            : Colors.transparent,
                                       ),
                                       child: makeFav
                                           ? Icon(
-                                        Icons.check,
-                                        color: Colors.white,
-                                        size: 20.0,
-                                      )
+                                              Icons.check,
+                                              color: Colors.white,
+                                              size: 20.0,
+                                            )
                                           : null,
                                     ),
                                     Padding(
@@ -310,32 +322,30 @@ class _CreateTimerState extends State<CreateTimer> {
                   ),
                 ),
               ),
-
               Expanded(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: GestureDetector(
-                            onTap: (){
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: GestureDetector(
+                        onTap: () {
 
-                                final todoBloc = BlocProvider.of<TodoBloc>(context);
-                                todoBloc.add(AddTodo(Todo(description: "${descriptiontext.text}", isCompleted: false, isFavorite: makeFav)));
-
-                                if(descriptiontext.text.isNotEmpty){
-                                  Navigator.pop(context);
-                                }else{
-
-
-                                }
-
-                            },
-                          child: Container(child: SvgPicture.asset(createbtn))),
-                    ),
-
-                  ],
-                )),
+                          if (descriptiontext.text.isNotEmpty) {
+                            Navigator.pop(context);
+                            final todoBloc = BlocProvider.of<TodoBloc>(context);
+                            todoBloc.add(AddTodo(Todo(
+                                description: "${descriptiontext.text}",
+                                isCompleted: false,
+                                isFavorite: makeFav)));
+                          } else {
+                            showMyCustomSnackBar(context,"Your Task Description must not be empty");
+                          }
+                        },
+                        child: Container(child: SvgPicture.asset(createbtn))),
+                  ),
+                ],
+              )),
             ],
           ),
         ),
--- lib/ui/widgets/snackBar.dart
+++ lib/ui/widgets/snackBar.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
 
-class MyWidget extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+void showMyCustomSnackBar(BuildContext context, String message) {
+  final snackBar = SnackBar(
+    content: Text(message),
+    duration: Duration(seconds: 2), // Adjust the duration as needed
+    backgroundColor: Colors.black87,
+    behavior: SnackBarBehavior.floating,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20),
+    ),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+
+class MySnackbar extends StatelessWidget {
+  String message;
+
+  MySnackbar({
+    required this.message,
+  });
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
@@ -23,4 +45,5 @@ class MyWidget extends StatelessWidget {
       child: Text('Show Snackbar'),
     );
   }
+
 }
