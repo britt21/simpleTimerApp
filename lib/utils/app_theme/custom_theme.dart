9abec1d50436835ec5d6083c872e7072c1f9a15a
bright <brittugu8@gmail.com>
2024-02-18 10:37:15 +0100

    Todo Details

---
+++ lib/utils/app_theme/custom_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: Color(0xFF214ECC),
+    backgroundColor: Color(0x49294487),
+    // Define more theme properties as needed
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    primaryColor: Color(0xFF214ECC),
+    backgroundColor: Color(0x49294487),
+    // Define more theme properties as needed
+  );
+}
